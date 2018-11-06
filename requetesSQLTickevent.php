a310c31a2bd0ae0e6675b30294adf6d52071fc50
Loic <loicguerin123@yahoo.fr>
2018-11-06 10:26:41 +0100

    feature: bdd connexion
    connexion de la bdd

--- requetesSQLTickevent.php
+++ requetesSQLTickevent.php
@@ -5,6 +5,15 @@
 	$host = "db759986335.hosting-data.io";
 	$port = "3306";
 	
+	$connexion = mysqli_connect($host, $user, $password, $port);
 
+	if ($connexion == true) 
+	{
+		echo "la base de données est connecté";
+	}
+	else 
+	{
+		echo "MERDE ALORS";
+	}
 
 ?>
